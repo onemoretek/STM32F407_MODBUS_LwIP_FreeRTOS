bd7f0a3b3ed09762e64432412c42a3894af87d82
Amoid <amoid@163.com>
2019-03-24 23:39:23 +0800

    Fixed ip order

--- Netapp/device_rpt.c
+++ Netapp/device_rpt.c
@@ -36,9 +36,9 @@ int dev_rpt_udp_disc_ack_pkt(const ip_addr_t *addr, unsigned short port)
    disc_rply.cmd = htonl(ugCmdDisc);
    disc_rply.dataLen = sizeof(dev_info_t);
    disc_rply.devinfo.sta = htonl(UG_SUCCESSED);
-   disc_rply.devinfo.ip = htonl(ip);
-   disc_rply.devinfo.msk = htonl(msk);
-   disc_rply.devinfo.gw = htonl(gw);
+   disc_rply.devinfo.ip = ip;//htonl(ip);
+   disc_rply.devinfo.msk = msk;//htonl(msk);
+   disc_rply.devinfo.gw = gw;//htonl(gw);
 
    for (i = 0; i < 6; i++) 
       disc_rply.devinfo.mac[i] = gnetif.hwaddr[i];
